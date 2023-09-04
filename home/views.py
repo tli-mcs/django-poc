47f4cd0bae66d5b37ffee7f9a59ae7670c5e5bc3
Ting Li <tli@macquariecloudservices.com>
2023-09-04 19:54:29 +1000

    initial

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'home/welcome.html',{'today':datetime.today()})
--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # apps
-    'home',
+    'home'
 ]
 
 MIDDLEWARE = [
